dbdf52186d3674bb35b88b6d51601926ab58c655
chweyun <chweyun>
2022-11-02 20:12:42 +0900

    setting

---
+++ client/src/pages/editor.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/scss/editor.scss'
+
+const editor = () => {
+    return (
+        <div>
+            <header>웹툰 블로그(로고)
+        
+            </header>
+            <section>
+                <div class="sidebar">하이</div>
+                <div class="editor">하이하이</div>
+            </section>
+        </div>
+    );
+};
+
+export default editor;
