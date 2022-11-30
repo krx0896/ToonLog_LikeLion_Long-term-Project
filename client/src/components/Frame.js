22affdab7a3b020fcd4958dff0beb5cf6162dc50
jeongwoo903 <www8565@naver.com>
2022-11-30 19:31:48 +0900

    Add: Frame

--- client/src/components/Frame.js
+++ client/src/components/Frame.js
@@ -1,10 +1,13 @@
+import Frame1 from "../images/frame/Frame1.svg"
+import Frame2 from "../images/frame/Frame2.svg"
 
 function Frame() {
   return (
     <>
       <section>
-        <div className="frame">
-          <img src="../images/frame/Frame1"></img>
+        <div className="frame_box">
+          <img src={Frame1} alt="Frame1"></img>
+          <img src={Frame2} alt="Frame1"></img>
         </div>
       </section>
     </>
