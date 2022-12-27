b648c4f29d3006b8df43c93749303104cf5ad328
jaqwe2301 <jaqwe2301@naver.com>
2022-12-27 21:25:28 +0900

    민혁 수정

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -54,57 +54,130 @@ const Editor = () => {
   }
 
   // -------------------------- 민혁 파트 -------------------------
-  
+
+  const [ ctxValue, setCtx ] = useState();
   const canvasId = React.useRef(null);
 
-  var x = 300;
-  var y = 300;
-  var width;
-  var height;
-  var canvas;
-  var ctx;
+  let locationSet = 217
+  var bubbleWidth = [584*0.3, 100, 200];
+  var bubbleHeight = [382*0.3, 100, 200];
+  var buubleTrue = [false]
+
+  var bubble1X = 0;
+  var bubble1Y = 0;
+
+  var mouseOn_x = 0;
+  var mouseOn_y = 0;
+
   const img = new Image();
-  img.src = "bubble/bubble1-squ.png";
-
-
-  const createBubble = (x, y, width, height) => {
-    var canvas = canvasId.current;
-    var ctx = canvas.getContext('2d');
-
-    ctx.clearRect(x, y, width, height);
-    ctx.beginPath();
-
-    ctx.drawImage(img, x, y, width, height);
-    
-    return (width, height)
-  }
-
-  const canvasOnmousedown = () => {
-    var canvas = canvasId.current;
-    var ctx = canvas.getContext('2d');
-
-
-    canvas.onmousedown = function(){
-      var canvas = canvasId.current;
-      var ctx = canvas.getContext('2d');
   
-      canvas.onmousemove = function(event){
-        
-        var x = event.clientX - ctx.canvas.offsetLeft;
-        var y = event.clientY - ctx.canvas.offsetTop;
-        
-        createBubble(x, y, width, height);
-      }
-    }
-  }
+  img.src = "bubble/bubble.png";
 
-  const removeBubble = () => {
+  useEffect(() => {
     var canvas = canvasId.current;
     var ctx = canvas.getContext('2d');
 
-    ctx.clearRect(x, y, 500, 500);
-    ctx.beginPath();
+    setCtx(ctx);
+
+    var sidebarWidth = document.querySelector('.sidebar').offsetWidth;
+
+   
+    canvas.onmousedown = (event) => {
+      mouseOn_x = event.clientX - sidebarWidth- ctx.canvas.offsetLeft;
+      mouseOn_y = event.clientY - 65 - ctx.canvas.offsetTop;
+
+      if ((buubleTrue[0] == true)
+        && (bubble1X-bubbleWidth[0]/2 <= mouseOn_x  && mouseOn_x <= bubble1X+bubbleWidth[0]/2)
+        && (bubble1Y-bubbleHeight[0]/2 <= mouseOn_y  && mouseOn_y <= bubble1X+bubbleHeight[0]/2)) {
+          console.log("mouseX : ", mouseOn_x)
+          console.log("mouseY : ", mouseOn_y)
+          console.log("bubble X : ", bubble1X-bubbleWidth[0]/2)
+          console.log("bubble Y : ", bubble1Y-bubbleHeight[0]/2)
+          
+
+          ctx.clearRect(bubble1X-bubbleWidth[0]/2, bubble1Y-bubbleHeight[0]/2, bubbleWidth[0], bubbleHeight[0]);
+
+          canvas.onmouseup = (event) => {
+            mouseOn_x = event.clientX - sidebarWidth- ctx.canvas.offsetLeft;
+            mouseOn_y = event.clientY - 65 - ctx.canvas.offsetTop;
+      
+            ctx.drawImage(img, mouseOn_x-bubbleWidth[0]/2, mouseOn_y-bubbleHeight[0]/2, bubbleWidth[0], bubbleHeight[0]);
+            console.log("draw X : ", mouseOn_x-bubbleWidth[0]/2, "draw Y : ", mouseOn_y-bubbleHeight[0]/2)
+            bubble1X=mouseOn_x;
+            bubble1Y=mouseOn_y;
+          }
+      }
+
+    }
+
+
+  });
+
+  const createBubble = () => {
+
+    // ctx.clearRect(locationSet, locationSet, bubbleWidth[0], bubbleWidth[0]);
+    ctxValue.drawImage(img, (locationSet-(bubbleWidth[0]/2)), (locationSet-(bubbleWidth[0]/2)), bubbleWidth[0], bubbleWidth[0]);
+
+    bubble1X = locationSet
+    bubble1Y = locationSet
+    buubleTrue[0] = true;
+
+    // ctx.beginPath();
+    // ctx.arc(x, y, 10, 0, 10*Math.PI);
+    // ctx.stroke();
+  
   }
+
+
+  // canvas.onmousedown = function(event){
+  //   var canvas = canvasId.current;
+  //   var ctx = canvas.getContext('2d');
+  //   var x = event.clientX - ctx.canvas.offsetLeft;
+  //   var y = event.clientY - ctx.canvas.offsetTop;
+
+  //   if (buubleTrue[0] == true
+  //     && (bubble1X-bubbleWidth[0]/2 <= x <=bubble1X+bubbleWidth[0]/2)
+  //     && (bubble1Y-bubbleHeight[0]/2 <= y <=bubble1Y+bubbleHeight[0]/2)) {
+      
+  //     console.log("yes")
+  //   }
+  // }
+
+  // const canvasOnmousedown = (event) => {
+  //   console.log();
+  //   var canvas = canvasId.current;
+  //   var ctx = canvas.getContext('2d');
+  //   var x = event.clientX - ctx.canvas.offsetLeft;
+  //   var y = event.clientY - ctx.canvas.offsetTop;
+
+  //   if (buubleTrue[0] == true
+  //     && (bubble1X-bubbleWidth[0]/2 <= x <=bubble1X+bubbleWidth[0]/2)
+  //     && (bubble1Y-bubbleHeight[0]/2 <= y <=bubble1Y+bubbleHeight[0]/2)) {
+      
+  //   }
+
+
+  //   canvas.onmousedown = function(){
+  
+  //     canvas.onmousemove = function(){
+        
+  //       // var x = event.clientX - ctx.canvas.offsetLeft;
+  //       // var y = event.clientY - ctx.canvas.offsetTop;
+        
+  //       ctx.clearRect(x, y, width, height);
+  //       createBubble(x, y, width, height);
+  //       // console.log(x, y);
+  //     }
+  //   }
+  // }
+
+  // const removeBubble = () => {
+  //   var canvas = canvasId.current;
+  //   var ctx = canvas.getContext('2d');
+
+  //   ctx.clearRect(x, y, 500, 500);
+  //   ctx.beginPath();
+  // }
 
   // -------------------------- 민혁 파트 끝 -------------------------
 
@@ -273,15 +346,15 @@ const Editor = () => {
         </div>
         <div className="l_IconBox iconBox">
           <button className="save" type="button"
-                  onClick={() => {
-                    canvasOnmousedown();
-                  }}
+                  // onClick={() => {
+                  //   canvasOnmousedown();
+                  // }}
                   >
             <img src={saveIcon} alt="save.png" />
           </button>
           <button className="share" type="button"
                   onClick={() => {
-                    createBubble(x, y, width=500, height=500);
+                    createBubble();
                   }}
                   >
             <img src={shareIcon} alt="share.png" />
@@ -317,12 +390,12 @@ const Editor = () => {
           <div className="editor">
             <body>
                 <canvas
-                width="1300"
-                height="1920"
+                width="433px"
+                height="640px"
                 ref={canvasId}
                 className="canvas"
                 id="canvas"
-                style={{ width: "433px", height: "640px", backgroundColor: "white" }} // * width:height 비율 꼭 1300:1920 비로 맞춰주세요!
+                style={{ width: "434px", height: "640px", backgroundColor: "white" }} // * width:height 비율 꼭 1300:1920 비로 맞춰주세요!
                 type="file"
                 name="imageFile"
                 accept="image/jpeg, image/jp, image/png"
